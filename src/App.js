fc562e3dc3ebc4dcf7911eb0f9a2f097f1b59141
Alfredo <alfredomeschis@yahoo.com>
2021-03-03 18:42:35 +0100

    add loading and cath error for API product

--- src/App.js
+++ src/App.js
@@ -3,27 +3,71 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import CarouselProducts from "./components/CarouselProducts";
 import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+// import { data } from "msw/lib/types/context";
 
 const fakeProducts = require("./mocks/data/products.json");
 
-const data = {
+const stateIn = {
   title: "Shop",
   description: "A fake e-commerce with a lot of potential",
   logo:
     "https://edgemony.com/wp-content/uploads/2020/03/cropped-Logo-edgemony_TeBIANCO-04.png",
   cover:
     "https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-  products: fakeProducts,
+  // products: fakeProducts,
 };
+
 
 function App() {
 
-  return <div className="App">
-    <Header src={data.logo} alt={`logo of ${data.title}`}/>
-  
-    <Hero src={data.cover} title={data.title} description={data.description}/>
+  const [data, setData] = useState(undefined)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
 
-    <CarouselProducts products={data.products}/>
+  
+  useEffect(() => {
+    
+    setLoading(true)
+
+      fetch('https://fakestoreapi.com/product')
+      .then(response => {
+        if (response.ok) {
+          response.json()
+        } else {
+          setLoading(false)
+          throw new Error('Something went wrong');
+        }
+      })
+      .then(data => {
+        setData(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.log(err)
+        setError(true)
+      })
+
+  }, [])
+  
+  return <div className="App">
+    <Header src={stateIn.logo} alt={`logo of ${stateIn.title}`}/>
+  
+    <Hero src={stateIn.cover} title={stateIn.title} description={stateIn.description}/>
+
+    {error ? 
+     <div className="error-container">
+       <span className="is-red">Ops.. </span>
+      Something went wrong.
+       </div>
+       : null} 
+
+    {data? <CarouselProducts products={data}/> : null}
+    
+    {loading ? <div className="circle-loading-container">
+      <div className="lds-facebook"><div></div><div></div><div></div></div>
+    </div>: null}
+
 
     <Footer/>
 
